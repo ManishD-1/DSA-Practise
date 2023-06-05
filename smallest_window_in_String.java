b6ce7c545dbb62a9dcc2e764a64a21c4363f2b2b
ManishD-1 <manishdeshpande121@gmail.com>
2023-06-05 19:50:32 +0530

    updated

--- smallest_window_in_String.java
+++ smallest_window_in_String.java
@@ -1,8 +1,7 @@
-import java.util.*;
+
 public class smallest_window_in_String {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        String p = sc.next();
+
+
     }
 }
